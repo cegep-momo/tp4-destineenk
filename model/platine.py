d7b2808eb47df7098b5430e55604bece4449b4a5
destineenk <2391753@cmontmorency.qc.ca>
2025-04-21 23:45:24 -0400

    boutonmesure, affichage chauqe 5 sec avec mesage mesure

--- model/platine.py
+++ model/platine.py
@@ -8,24 +8,41 @@ class Platine:
         self.bouton_demarrer = Button(23) # permet de demarrer et arreter le systeme
         self.bouton_mesure = Button(24) # permet de orendre la mesure
         self.systeme_demarrer = False # systeme en marche
-        LCD1602.init(0x27,1) #initialisation de ecran
-        self.capteur = DistanceSensor(echo = 12, trigger = 17, max_distance = 3)
+        self.mesure = False
+        self.lcd = LCD1602
+        self.lcd.init(0x27,1) #initialisation de ecran
+        self.capteur = DistanceSensor(echo = 12, trigger = 17)
     
     def appuyer_bouton(self):
         while True:
             if self.bouton_demarrer.is_pressed:
-                if not self.systeme_demarrer:
-                    self.systeme_demarrer = True
-                    print("Le systeme est demarre")
-                    self.cm = self.capteur.distance * 100
-                    LCD1602.write(0x0, "Distance")
-                    LCD1602.write(0x1, f"{self.cm} cm")
-                    
-                    sleep(1)
-                    
+                self.systeme_demarrer = not self.systeme_demarrer
+                if self.systeme_demarrer:
+                    self.lcd.write(0,0, "Systeme actif")
+                    self.lcd.write(1,1, " ")                              
                 else:
-                    print("Le systeme est deja en cours")
+                    self.lcd.write(0,0, "Systeme arrete")
+                    self.lcd.write(1,1, " ")
+                    self.mesure = False
+                sleep(1)
+                
+            if self.systeme_demarrer and self.bouton_mesure.is_pressed:
+                self.lcd.write(0,0, "Mesure active")
+                self.lcd.write(1,1, "    ")
+                sleep(1)
+                self.mesure = True
+                  
+                while self.bouton_mesure.is_pressed:
+                    sleep(0.5)
                 sleep(0.5)
-            sleep(0.5)
-            
+                
+            if self.systeme_demarrer and self.mesure:
+                self.cm = round(self.capteur.distance * 100)
+                self.lcd.write(0,0, "Distance")
+                self.lcd.write(1,1, f"{self.cm} cm")
+                sleep(5)
+            sleep(0.1)
+                    
+                
+                            
         
